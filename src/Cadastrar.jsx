ef84515395ff33f0265d5e9c8eda2823c00a4fe8
douglaswe <djfq13@hotmail.com>
2024-05-10 11:43:50 -0300

    atualizações

--- src/Cadastrar.jsx
+++ src/Cadastrar.jsx
@@ -9,22 +9,19 @@ export function Cadastrar(){
   };
 
   return (
-    <div className="container">
-      <form className="form" onSubmit={handleSubmit}> 
-       <h1 className="title">Cadastrar</h1>
-        <input className="input" type="text" name="nome" required placeholder="Nome" />
-        <input className="input" type="text" name="sobrenome" required placeholder="Sobrenome" />
-        <input className="input" type="email" name="email" required placeholder="Email" />
-        <input className="input" type="password" name="senha" required placeholder="Senha" />
+    <div className="container-cadastrar">
+      <form className="form-cadastrar" onSubmit={handleSubmit}> 
+       <h1 className="title-cadastrar">Cadastrar</h1>
+        <input className="input-cadastrar" type="text" name="nome" required placeholder="Nome" />
+        <input className="input-cadastrar" type="text" name="sobrenome" required placeholder="Sobrenome" />
+        <input className="input-cadastrar" type="email" name="email" required placeholder="Email" />
+        <input className="input-cadastrar" type="password" name="senha" required placeholder="Senha" />
         <label style={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
           <input type="checkbox" name="allow" />
           Eu aceito os Termos de uso
         </label>
-        <button className="button" type="submit">Cadastrar</button>
-        <p className="signup-link"><Link to="/">Crie uma!</Link></p>
-        <p >
-          Já tem uma conta ? <a href="#">Login</a>
-        </p>
+        <button className="button-cadastrar" type="submit">Cadastrar</button>
+        <Link className="signup-cadastrar" to="/">Crie uma!</Link>
       </form>
     </div>
   );
