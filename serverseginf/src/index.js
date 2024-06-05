2cf308a7e2922ffe788b09366b4b819c110f935e
ciscoquirino <quirino1988@gmail.com>
2024-06-05 10:52:33 -0300

    add server

---
+++ serverseginf/src/index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const app = express();
+const mysql = require('mysql2');
+
+app.get('/', function (req, res) {
+    res.send('Raiz');
+});
+
+const db = mysql.createConnection({
+    host: 'localhost',    
+    user: 'root',         
+    password: 'ciscoTr3t@',         
+    database: 'seginf'  
+});
+
+db.connect((err) => {
+    if (err) {
+        throw err;
+    }
+    console.log('Connected to the database');
+});
+
+app.get('/getusers', (req, res) => {
+    const sql = 'SELECT * FROM usuario';
+    
+    db.query(sql, (err, results) => {
+      if (err) throw err;
+      res.json(results);
+    });
+});
+
+app.get('/gettermo', (req, res) => {
+    const sql = 'SELECT * FROM termo WHERE ter_data = (SELECT MAX(ter_data) FROM termo);';
+    
+    db.query(sql, (err, results) => {
+      if (err) throw err;
+      res.json(results);
+    });
+});
+
+
+
+
+app.listen(3003, function () {
+    console.log("Servidor rodando na porta 3003...");
+});
