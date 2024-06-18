dfb8de66eddb64d60e6ce0b9f16d566a2dc8e883
ciscoquirino <quirino1988@gmail.com>
2024-06-18 18:35:25 -0300

    cadastro de usuario com aceite de termo e opcionais

--- front/src/Cadastrar.jsx
+++ front/src/Cadastrar.jsx
@@ -8,7 +8,8 @@ export function Cadastrar() {
       event.preventDefault();
       const formData = new FormData(event.target);
       const data = Object.fromEntries(formData.entries());
-  
+      data.termo_id = termo.ter_id
+      console.log(data);
       try {
           const response = await fetch('http://localhost:3003/cadastrar', {
               method: 'POST',
@@ -22,7 +23,7 @@ export function Cadastrar() {
               const errorMessage = await response.text();
               throw new Error(errorMessage);
           }
-  
+          
           console.log('Usuário cadastrado com sucesso!');
           // Aqui você pode redirecionar para outra página ou mostrar uma mensagem de sucesso
       } catch (error) {
@@ -32,7 +33,8 @@ export function Cadastrar() {
   };
   
 
-  const [termos, setTermos] = useState([]);
+  const [termo, setTermo] = useState({});
+  const [opcionais, setOpcionais] = useState([]);
   const [error, setError] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -46,8 +48,8 @@ export function Cadastrar() {
           throw new Error(errorMessage);
         }
         const data = await response.json();
-        setTermos(data);
-        console.log(data);
+        setTermo(data);
+        setOpcionais(termo.ter_opcionais);
       } catch (error) {
         console.error('Erro ao buscar termos:', error);
         setError(error.message);
@@ -73,7 +75,7 @@ export function Cadastrar() {
     return date.toLocaleDateString(undefined, options);
   };
 
-  const primeiroTermo = termos.length > 0 ? termos[0] : null;
+  const temOpc = opcionais.length > 0 ? opcionais : null;
   
   return (
     <div className="container-cadastrar">
@@ -84,16 +86,17 @@ export function Cadastrar() {
         <input className="input-cadastrar" type="email" name="email" required placeholder="Email" />
         <input className="input-cadastrar" type="password" name="senha" required placeholder="Senha" />
         <label style={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
-          <input type="checkbox" style={{ cursor: 'pointer' }} name="allow" required />
+          <input type="checkbox" style={{ cursor: 'pointer' }} name="termo" required />
           <p onClick={abrirModal} style={{ cursor: 'pointer', textDecoration: 'underline' }}>Eu aceito os Termos de uso</p>
         </label>
-
-        {!isLoading && primeiroTermo && (
-          <label style={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
-            <input type="checkbox" style={{ cursor: 'pointer' }} name="termo" required />
-            <p>{primeiroTermo.opc_texto}</p>
+        {!isLoading && temOpc && (
+          opcionais.map((item) => (
+            <label key={item.opc_id} style={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
+            <input type="checkbox" style={{ cursor: 'pointer' }} name={"opc " + item.opc_id} />
+            <p>{item.opc_texto}</p>
           </label>
-        )}
+          ))
+        )};
 
         <button className="button-cadastrar" type="submit">Cadastrar</button>
         <Link className="signup-cadastrar" to="/">Já tem uma conta? Entrar</Link>
@@ -104,12 +107,8 @@ export function Cadastrar() {
           <div className="modal-content">
             <span className="close" onClick={fecharModal}>&times;</span>
             <div>
-              {termos.length > 0 ? termos.map((item) => (
-                <div key={item.ter_id}>
-                  <h3>{item.ter_texto}</h3>
-                  <p>Data do termo: {formatarData(item.ter_data)}</p>
-                </div>
-              )) : error && <p className="error">{error}</p>}
+                  <h3>{termo.ter_texto}</h3>
+                  <p>Data do termo: {formatarData(termo.ter_data)}</p>
             </div>
           </div>
         </div>
--- serverseginf/src/index.js
+++ serverseginf/src/index.js
@@ -17,6 +17,7 @@ const db = new Pool({
 db.connect((err) => {
     if (err) {
         throw err;
+        console.log(err)
     }
     console.log('Connected to the database');
 });
@@ -30,20 +31,66 @@ app.get('/getusers', (req, res) => {
     });
 });
 
-app.get('/gettermo', (req, res) => {
-    const sql = `SELECT * FROM termo t
-                JOIN opcional o 
-                ON o.opc_id_termo = t.ter_id
-                WHERE t.ter_data = (SELECT MAX(ter_data) FROM termo);`;
-    
-
-    db.query(sql, (err, results) => {
-      if (err) throw err;
-      res.json(results.rows);
-    });
+app.get('/gettermo', async (req, res) => {
+    try{
+        const termo = await db.query(`SELECT * FROM termo WHERE ter_data = (SELECT MAX(ter_data) FROM termo);`);
+        const id_termo = termo.rows[0].ter_id
+        const opc = await db.query(`SELECT opc_id, opc_texto FROM opcional WHERE opc_id_termo = ${id_termo};`);
+        const resultado = {
+            ter_id: termo.rows[0].ter_id,
+            ter_versao: termo.rows[0].ter_versao,
+            ter_texto: termo.rows[0].ter_texto,
+            ter_data: termo.rows[0].ter_data,
+            ter_opcionais: opc.rows
+        }
+        if(termo.rowCount == 0){
+            res.status(404).json({ message: 'dados não encontrados' });
+        }else{
+            res.status(200).json(resultado);
+        }
+    }catch (error) {
+        res.status(500).json({ error: error.message });
+    }   
 });
 
+app.post('/cadastrar', async (req,res) => {
+    try {
+        const { nome , email , telefone , senha, termo_id} = req.body;
+        const keys = Object.keys(req.body);
+        const opcAceitados = new Array;
 
+        const testeEmail = await db.query(`SELECT * FROM usuario WHERE usu_email = '${email}'`);
+
+        if(testeEmail.rowCount > 0){
+            res.status(500).json({ message: "E-mail já cadastrado" });
+        }else{
+            await db.query('INSERT INTO usuario(usu_nome, usu_email, usu_telefone, usu_senha) VALUES($1, $2, $3, $4)',
+            [nome, email, telefone, senha]);
+
+            const queryUserId = await db.query(`SELECT usu_id FROM usuario WHERE usu_email = '${email}' `);
+            const userId = Number(queryUserId.rows[0].usu_id);
+
+            await db.query(`INSERT INTO aceite_termo(ace_id_usuario, ace_id_termo, ace_aceitado)
+                VALUES(${userId}, ${termo_id}, TRUE)`);
+
+            for(let i = 0; i< keys.length; i++){
+                if(keys[i].startsWith('opc')){
+                    const opc_id = keys[i].split(' ')[1];
+                    opcAceitados.push(Number(opc_id))
+                }
+            }
+            for(let i = 0; i< opcAceitados.length; i++){
+                db.query(`INSERT INTO aceite_opc(aop_id_usuario, aop_id_termo, aop_id_opcional, aop_aceitado)
+                    VALUES(${userId}, ${termo_id}, ${opcAceitados[i]}, TRUE)`);
+            }
+            res.status(201).json({ message: 'usuário criado com sucesso' });
+        }
+        
+      } catch (error) {
+        res.status(500).json({ error: error.message });
+      }
+});
+  
 
 app.listen(3003, function () {
     console.log("Servidor rodando na porta 3003...");
